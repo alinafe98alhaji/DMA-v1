dcb385a1c5d1ef9ac61e379ca99eba28a24df93b
alinafe98alhaji <emmanueldicksonmapulanga@gmail.com>
2024-12-06 15:22:18 +0200

    uyg

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,13 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  
   // Other config options here
 };
-
+module.exports = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
+}
 export default nextConfig;
 
